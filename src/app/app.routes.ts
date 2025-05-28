934d34b501d8385a09e8d456cba559d8aec3bb2e
Jeffrod1098 <mrjeffrey1011@gmail.com>
2025-05-27 22:19:44 -0400

    landing, signin page created and routed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SigninComponent },
+  { path: 'landing', component: LandingComponent }
+];
